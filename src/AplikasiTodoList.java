c7ba46b3faa532b65cc2ca0f3826cc8a6d980038
Andrey Sholinka <sholinka2111@gmail.com>
2023-03-01 11:53:01 +0700

    model-business logic

---
+++ src/AplikasiTodoList.java
@@ -0,0 +1,31 @@
+public class AplikasiTodoList {
+
+    public static String[] model = new String[10];
+
+    public static void main(String[] args) {
+
+    }
+
+    //model - business logic
+
+    /**
+     * Menampilan toto list
+     */
+    public static void showTodoList(){
+
+    }
+
+    /**
+     * Menambah todo to list
+     */
+    public static void addTodoList(){
+
+    }
+
+    /**
+     * Menghapus todo list
+     */
+    public static void removeTodoList(){
+
+    }
+}
